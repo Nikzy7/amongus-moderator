82dffcd058ebc30e5f1f712b566463facb6c45b5
Cahllagerfeed <jkarl0710@gmail.com>
2020-12-08 18:45:20 +0100

    Revert "Discord Token to Environment"
    This reverts commit 48be49efd6288e2cf7b4ed7bb9cd46e13455e485.

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-require("dotenv").config();
 const { Client } = require("discord.js");
 
 // Declares our bot,
 // the disableEveryone prevents the client to ping @everyone
 const client = new Client({
-  disableEveryone: true,
+    disableEveryone: true
 });
 
 config({
-  path: __dirname + "/.env",
-});
+    path: __dirname + "/.env"
+})
 
 // When the bot's online, what's in these brackets will be executed
 client.on("ready", () => {
-  // Set the user presence
-  client.user.setPresence({
-    status: "online",
-    game: {
-      name: "me getting developed",
-      type: "WATCHING",
-    },
-  });
-});
+    // Set the user presence
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    }); 
+})
 
-client.on("message", async (message) => {
-  if (message.author.bot && !(message.author.username == "Manager")) return;
-  if (!message.guild) return;
 
-  let channel = client.channels.cache.get("ENTER_CHANNEL_ID_HERE");
+client.on("message", async message => {
+    
+    if(message.author.bot && !(message.author.username == "Manager")) return;
+    if (!message.guild) return;
 
-  if (message.author.username == "Manager" && message.author.bot) {
-    if (message.content == "/muteAll") {
-      for (let member of channel.members) {
-        await member[1].voice.setMute(true);
-      }
-      await message.channel.send(
-        "Sssshhhhhhhhhh ! Users muted by the Moderator !"
-      );
+    let channel = client.channels.cache.get('ENTER_CHANNEL_ID_HERE');
+
+    if(message.author.username == "Manager" && message.author.bot){
+    if (message.content == '/muteAll') {  
+        for (let member of channel.members) {
+            await member[1].voice.setMute(true);
+        }
+        await message.channel.send('Sssshhhhhhhhhh ! Users muted by the Moderator !');
+     }
+
+    if (message.content == '/unmuteAll')
+    {
+        for (let member of channel.members) {
+            await member[1].voice.setMute(false);
+        }
+        await message.channel.send('Discuss!!!!! Users unmuted by the Moderator !');
     }
-
-    if (message.content == "/unmuteAll") {
-      for (let member of channel.members) {
-        await member[1].voice.setMute(false);
-      }
-      await message.channel.send(
-        "Discuss!!!!! Users unmuted by the Moderator !"
-      );
-    }
-  }
+}
 });
 
 // Login the bot
-client.login(process.env.DISCORD_TOKEN);
+client.login('ENTER_TOKEN_HERE');
